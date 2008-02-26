deb531c63756f1b52081425bed65c4dd859e0954
sally <sally@a03d2bdc-e443-11dc-a185-ff1495ed1cd9>
2008-02-26 10:46:12 +0000

    Implemented some functionality into fib.c

--- src/fib.c
+++ src/fib.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int fib(int n) {
-    printf("fib: unimplemented!\n");
-    return -1;
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	printf("fib(%d): unimplemented!\n", n);
+	return -1;
 }
